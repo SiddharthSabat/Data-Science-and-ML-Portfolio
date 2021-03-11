ccaa4f5c66a849bbba0237f1d53929c964d69ac1
SiddharthSabat <siddharthsabat@gmail.com>
2021-03-11 20:03:47 +0400

    ETL Pipleline complete

--- Disaster Response Project/data/process_data.py
+++ Disaster Response Project/data/process_data.py
@@ -1,19 +1,111 @@
-import sys
+"""
+DAYA PRE-PROCESSING
+Disaster Response Pipeline Project as part of Udacity Data Science Nanodegree program.
 
+Sample Script Execution Command:
+> python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db
+
+Arguments:
+    1) 1st CSV file containing the messages data (disaster_messages.csv)
+    2) 2nd CSV file containing categories data (disaster_categories.csv)
+    3) SQLite destination database (DisasterResponse.db)
+"""
+
+import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    """
+    Import data from CSV files and combine both datasets into a single dataframe.
+    
+    Input:
+        messages_filepath -> File path to disaster_messages.csv file
+        categories_filepath -> File path to disaster_categories.csv file
+    Output:
+        df -> A new dataframe merged with disaster_messages and disaster_categories data.
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on = 'id')
+    return df 
 
 def clean_data(df):
-    pass
+    """
+    Clean the merged dataframe by splitting categories into separate category columns and converting categoroes to 0 or 1
 
+    
+    Arguments:
+        df -> Merged df with the two datasets
+    Outputs:
+        df -> clean dataframe with category values converted into numbers 0 or 1 and with duplicate entries removed.
+    """
+    categories = df.categories.str.split(pat=';',expand=True)
+        
+    #cols = []
+    #i = 0
+    #name = categories.iloc[:1, :categories.shape[1]]
+    #for columns in name:
+        #cols.append(name.iloc[:1, :categories.shape[1]][i].str.split("-")[0][0])
+        #i = i+1
+    #category_colnames = col 
+    #categories.columns = category_colnames
+    
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x:x[:-2])     
+    
+    # rename the columns of categories
+    categories.columns = category_colnames      
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] =  categories[column].str[-1]
+    
+        # convert column from string to numeric
+        categories[column] =  categories[column].astype('int')
+    
+    # drop the original categories column from `df`
+    df.drop('categories', axis =1, inplace =True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+    
+    # drop duplicates
+    df.drop_duplicates(inplace = True)
+    
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save the clean dataset into an sqlite database
+    
+    input:
+        df -> Clean DataFrame
+        database_filename -> database file (.db) destination path
+    """
+    
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('Disaster_response', engine, index=False,  if_exists="replace")
+
+    pass 
 
 
 def main():
+    """
+    Main Data Processing function
+    
+    This function implements the ETL pipeline:
+        1) Data extraction from .csv files
+        2) Data cleaning and pre-processing
+        3) Data loading to SQLite database
+    """
+    print(sys.argv)
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
